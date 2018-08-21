725025821321ca0638442411be873b9468a5dd41
Atrey Gajjar <gajjar_atrey98@hotmail.com>
2018-08-22 01:00:37 +1000

    integrating semantic ui

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,11 +1,14 @@
 window.addEventListener("load", initMap, false);
 
 function initMap() {
-    var map = L.map("map").setView([51.505, -0.09], 13);
+    var map = L.map("map", {
+        zoomDelta: 0.25,
+        zoomSnap: 0
+    }).setView([55, 100], 10);
     L.tileLayer(
         "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
         {
-            maxZoom: 18,
+            maxZoom: 10,
             attribution:
                 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                 '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
